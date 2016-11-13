7ba1044731cee6e022fe1790108026fec2de3482
depeckerd <denis.depecker@cgi.com>
2016-11-13 14:05:14 +0100

    Ajouts des tests pour le croupier.

--- atelier2/src/main/java/dad/atelier2/table/Croupier.java
+++ atelier2/src/main/java/dad/atelier2/table/Croupier.java
@@ -3,12 +3,9 @@ package dad.atelier2.table;
 import java.util.ArrayList;
 import java.util.List;
 
-import dad.atelier2.exception.MainPleineException;
 import dad.atelier2.joueur.Joueur;
 
 public class Croupier {
-
-	private static final int TAILLE_MAX_MAIN = 5;
 
 	private List<Joueur> joueurs;
 	private Paquet paquet;
@@ -22,15 +19,17 @@ public class Croupier {
 		joueurs.add(joueur);
 	}
 
+	public int getNombreJoueurs() {
+		return joueurs.size();
+	}
+
 	public void distribuer() {
-		try {
-			for (int i = 0; i < TAILLE_MAX_MAIN; ++i) {
-				for (Joueur j : joueurs) {
-					j.recevoirCarte(paquet.tirerCarte());
-				}
+		boolean mainPleine = false;
+		while (!mainPleine) {
+			for (Joueur j : joueurs) {
+				j.recevoirCarte(paquet.tirerCarte());
+				mainPleine = j.montrerMain().isPleine();
 			}
-		} catch (MainPleineException e) {
-			System.out.println("Trop de carte, tant pis !");
 		}
 	}
 
--- atelier2/src/test/java/dad/atelier2/table/CroupierTest.java
+++ atelier2/src/test/java/dad/atelier2/table/CroupierTest.java
@@ -4,11 +4,37 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import dad.atelier2.joueur.Joueur;
+
 public class CroupierTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAccueillirJoueur() {
+		Croupier c = new Croupier();
+		assertEquals("Il n'y a pas de joueur pour le moment.", 0, c.getNombreJoueurs());
+		c.accueillir(new Joueur("toto"));
+		assertEquals("Il n'y a pas de joueur pour le moment.", 1, c.getNombreJoueurs());
+		c.accueillir(new Joueur("tata"));
+		assertEquals("Il n'y a pas de joueur pour le moment.", 2, c.getNombreJoueurs());
+	}
+
+	@Test
+	public void testDistribuer() {
+		Croupier c = new Croupier();
+		Joueur[] joueurs = new Joueur[] { new Joueur("toto"), new Joueur("tata"), new Joueur("titi") };
+		for (Joueur j : joueurs) {
+			c.accueillir(j);
+		}
+
+		for (Joueur j : joueurs) {
+			assertTrue("Chaque joueur a une main vide", j.montrerMain().isVide());
+		}
+
+		c.distribuer();
+
+		for (Joueur j : joueurs) {
+			assertTrue("Chaque joueur a une main pleine", j.montrerMain().isPleine());
+		}
 	}
 
 }
