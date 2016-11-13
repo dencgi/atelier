641586126a25e9996b1fe38c1a587a95f8614361
depeckerd <denis.depecker@cgi.com>
2016-11-13 15:22:53 +0100

    Finalisation du croupier.

--- atelier2/src/main/java/dad/atelier2/table/Croupier.java
+++ atelier2/src/main/java/dad/atelier2/table/Croupier.java
@@ -1,18 +1,28 @@
 package dad.atelier2.table;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import dad.atelier2.combinaison.Combinaison;
 import dad.atelier2.joueur.Joueur;
 
 public class Croupier {
 
 	private List<Joueur> joueurs;
 	private Paquet paquet;
+	private Map<Combinaison, List<Joueur>> classement;
 
 	public Croupier() {
 		joueurs = new ArrayList<>();
 		paquet = new Paquet();
+		classement = new LinkedHashMap<>();
+
+		// Initialisation du classement des joueurs.
+		for (Combinaison combinaison : Combinaison.valuesDescendingSort()) {
+			classement.put(combinaison, new ArrayList<>());
+		}
 	}
 
 	public void accueillir(Joueur joueur) {
@@ -31,6 +41,26 @@ public class Croupier {
 				mainPleine = j.montrerMain().isPleine();
 			}
 		}
+	}
+
+	private void determinerClassement() {
+		for (List<Joueur> joueursClassement : classement.values()) {
+			joueursClassement.clear();
+		}
+		for (Joueur joueur : joueurs) {
+			List<Joueur> joueursClassement = classement.get(Combinaison.getCombinaison(joueur.montrerMain()));
+			joueursClassement.add(joueur);
+		}
+	}
+
+	public List<Joueur> getGagnants() {
+		determinerClassement();
+		for (List<Joueur> joueurs : classement.values()) {
+			if (!joueurs.isEmpty()) {
+				return joueurs;
+			}
+		}
+		return new ArrayList<>();
 	}
 
 	@Override
--- atelier2/src/test/java/dad/atelier2/table/CroupierTest.java
+++ atelier2/src/test/java/dad/atelier2/table/CroupierTest.java
@@ -2,8 +2,13 @@ package dad.atelier2.table;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
+import dad.atelier2.carte.Carte;
+import dad.atelier2.carte.Couleur;
+import dad.atelier2.carte.Valeur;
 import dad.atelier2.joueur.Joueur;
 
 public class CroupierTest {
@@ -35,6 +40,71 @@ public class CroupierTest {
 		for (Joueur j : joueurs) {
 			assertTrue("Chaque joueur a une main pleine", j.montrerMain().isPleine());
 		}
+	}
+
+	@Test
+	public void testDeterminerGagnant() {
+		Croupier c = new Croupier();
+		Joueur[] joueurs = new Joueur[] { new Joueur("toto"), new Joueur("tata"), new Joueur("titi") };
+		for (Joueur j : joueurs) {
+			c.accueillir(j);
+		}
+		c.distribuer();
+
+		List<Joueur> gagnants = c.getGagnants();
+		assertFalse("Il y a au moins un gagnant.", gagnants.isEmpty());
+	}
+
+	@Test
+	public void testGagnantMainIdentique() {
+		Croupier c = new Croupier();
+		Joueur[] joueurs = new Joueur[] { new Joueur("toto"), new Joueur("tata"), new Joueur("titi") };
+		for (Joueur j : joueurs) {
+			c.accueillir(j);
+			j.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.DIX));
+			j.recevoirCarte(new Carte(Couleur.COEUR, Valeur.SIX));
+			j.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.AS));
+			j.recevoirCarte(new Carte(Couleur.TREFLE, Valeur.DIX));
+			j.recevoirCarte(new Carte(Couleur.PIQUE, Valeur.DEUX));
+		}
+
+		List<Joueur> gagnants = c.getGagnants();
+		assertEquals("Il y a trois gagnants.", 3, gagnants.size());
+	}
+
+	@Test
+	public void testGagnantToto() {
+		Croupier c = new Croupier();
+		Joueur[] joueurs = new Joueur[] { new Joueur("toto"), new Joueur("tata"), new Joueur("titi") };
+		
+		Joueur toto = joueurs[0];
+		toto.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.DIX));
+		toto.recevoirCarte(new Carte(Couleur.COEUR, Valeur.DIX));
+		toto.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.AS));
+		toto.recevoirCarte(new Carte(Couleur.TREFLE, Valeur.DIX));
+		toto.recevoirCarte(new Carte(Couleur.PIQUE, Valeur.DEUX));
+		
+		Joueur tata = joueurs[1];
+		tata.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.DIX));
+		tata.recevoirCarte(new Carte(Couleur.COEUR, Valeur.SIX));
+		tata.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.AS));
+		tata.recevoirCarte(new Carte(Couleur.TREFLE, Valeur.DIX));
+		tata.recevoirCarte(new Carte(Couleur.PIQUE, Valeur.DEUX));
+		
+		Joueur titi = joueurs[2];
+		titi.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.DIX));
+		titi.recevoirCarte(new Carte(Couleur.COEUR, Valeur.SIX));
+		titi.recevoirCarte(new Carte(Couleur.CARREAU, Valeur.AS));
+		titi.recevoirCarte(new Carte(Couleur.TREFLE, Valeur.ROI));
+		titi.recevoirCarte(new Carte(Couleur.PIQUE, Valeur.DEUX));
+
+		for (Joueur j : joueurs) {
+			c.accueillir(j);
+		}
+
+		List<Joueur> gagnants = c.getGagnants();
+		assertEquals("Il y a un seul gagnant.", 1, gagnants.size());
+		assertEquals("Le gagnant est toto.", "toto", gagnants.get(0).getNom());
 	}
 
 }
