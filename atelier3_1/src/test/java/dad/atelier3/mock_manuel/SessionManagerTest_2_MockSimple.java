cc21614244da1bdc3abd774106a48b22957b335b
depeckerd <denis.depecker@cgi.com>
2017-05-16 13:46:30 +0200

    Supression du @PostConstruct. Ajout d'un test qui échoue.

--- atelier3_1/src/test/java/dad/atelier3/mock_manuel/SessionManagerTest_2_MockSimple.java
+++ atelier3_1/src/test/java/dad/atelier3/mock_manuel/SessionManagerTest_2_MockSimple.java
@@ -2,6 +2,7 @@ package dad.atelier3.mock_manuel;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import javax.annotation.PostConstruct;
@@ -31,7 +32,7 @@ public class SessionManagerTest_2_MockSimple {
 	@Autowired
 	private Vue vue;
 
-	@PostConstruct
+	@Before
 	public void mock() {
 		SessionManager sessionManagerMock = new SessionManager() {
 			public Session getCurrentSession() {
@@ -41,26 +42,33 @@ public class SessionManagerTest_2_MockSimple {
 		ReflectionTestUtils.setField(vue, "sessionManager", sessionManagerMock);
 	}
 
-	@Before
-	public void before() {
-		vue.start();
-	}
-
-	@After
-	public void after() {
-		vue.done();
-	}
-
 	@Test
 	public void bonneCommandeInterimaireTest() {
+		vue.start();
 		vue.traitement("int-nom-azerty");
 		vue.traitement("int-prenom-titi");
+		vue.done();
 
 		assertEquals("Il s'agit d'un intérimaire", Individu.Type.INTERIMAIRE, vue.getIndividu().getType());
 		assertNotEquals("Le nom n'a pas été remplacé", "azerty", vue.getIndividu().getNom());
 		assertNotEquals("Le nom n'a pas été remplacé", "titi", vue.getIndividu().getPrenom());
 		assertTrue("Le nom a été ajouté", vue.getIndividu().getNom().endsWith("azerty"));
 		assertTrue("Le prénom a été ajouté", vue.getIndividu().getPrenom().endsWith("titi"));
+	}
+
+	@Test
+	public void ancienneBonneCommandeInterimaireTest() {
+		vue.start();
+		vue.traitement("int-nom-azerty");
+		Individu ind1 = vue.getIndividu();
+		vue.traitement("int-prenom-titi");
+		Individu ind2 = vue.getIndividu();
+		vue.done();
+
+		assertSame("Il s'agit d'un seul et même individu", ind1, ind2);
+		assertEquals("Il s'agit d'un intérimaire", Individu.Type.INTERIMAIRE, vue.getIndividu().getType());
+		assertEquals("Le nom a été remplacé", "azerty", vue.getIndividu().getNom());
+		assertEquals("Le prénom a été remplacé", "titi", vue.getIndividu().getPrenom());
 	}
 
 }
